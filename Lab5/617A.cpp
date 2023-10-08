a5485677e6fa28e35904bc5a48ccfb042ea976fe
Shamim Hossan <141517665+shamimOSD@users.noreply.github.com>
2023-10-08 12:06:19 +0600

    Add files via upload

---
+++ Lab5/617A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int e;
+        cin>>e;
+        if(e%5==0)
+        {
+                cout<<(e/5)<<endl;
+        }
+        else
+        {
+                cout<<(e/5)+1<<endl;
+        }
+        return 0;
+}
